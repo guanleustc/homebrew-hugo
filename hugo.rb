bfeaf4f16809d0d65e13509cc66b6f45aae0a6bb
guanleustc <guanle.ustc@gmail.com>
2020-12-22 14:52:13 -0500

    hugo 0.71.0

---
+++ hugo.rb
@@ -0,0 +1,42 @@
+class Hugo < Formula
+  desc "Configurable static site generator"
+  homepage "https://gohugo.io/"
+  url "https://github.com/gohugoio/hugo/archive/v0.71.0.tar.gz"
+  sha256 "a0ddf9b09567d13c66b12051c33143c957c401a51bf46d6257378d342fd9d4b4"
+  head "https://github.com/gohugoio/hugo.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f757750c457e226d7164f8861242daf09ce0e88035d90c7d48985ec8c2c65ded" => :catalina
+    sha256 "aab3dd76bbf92bc451ae7dd22d7e27ba4065547dbaf7942c04a0080ad4935da4" => :mojave
+    sha256 "92c200ad8e1a6352ce53500757ec3634206b327eac994d8ed5245946ef5d15ed" => :high_sierra
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    (buildpath/"src/github.com/gohugoio/hugo").install buildpath.children
+
+    cd "src/github.com/gohugoio/hugo" do
+      system "go", "build", "-o", bin/"hugo", "-tags", "extended", "main.go"
+
+      # Build bash completion
+      system bin/"hugo", "gen", "autocomplete", "--completionfile=hugo.sh"
+      bash_completion.install "hugo.sh"
+
+      # Build man pages; target dir man/ is hardcoded :(
+      (Pathname.pwd/"man").mkpath
+      system bin/"hugo", "gen", "man"
+      man1.install Dir["man/*.1"]
+
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    site = testpath/"hops-yeast-malt-water"
+    system "#{bin}/hugo", "new", "site", site
+    assert_predicate testpath/"#{site}/config.toml", :exist?
+  end
+end
